d252330a6a18604bb81fb56527ade48e730cdc0e
ArshiaAHP <diniodduck@gmail.com>
2025-03-26 22:34:07 +0330

    add validators to Database.

--- src/db/Database.java
+++ src/db/Database.java
@@ -1,20 +1,25 @@
 package db;
 
 import db.exception.EntityNotFoundException;
+import db.exception.InvalidEntityException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.UUID;
 
 public class Database {
 
     private static ArrayList<Entity> entities = new ArrayList<>();
+    private static HashMap<Integer, Validator> validators = new HashMap<>();
 
     private Database() {}
 
-    public static void add(Entity e){
+    public static void add(Entity e) throws InvalidEntityException{
+        Validator validator = validators.get(e.getEntityCode());
+        validator.validate(e);
         e.id = UUID.randomUUID();
-        System.out.println("Entity added successfully. id: " + e.id);
-        entities.add(e.copy());
+        entities.add(e);
+        System.out.println("Entity added successfully.");
     }
 
     public static Entity get(UUID id) throws EntityNotFoundException{
@@ -37,10 +42,12 @@ public class Database {
         throw new EntityNotFoundException("Entity not found to delete.");
     }
 
-    public static void update(Entity entityInput) throws EntityNotFoundException{
+    public static void update(Entity entityInput) throws EntityNotFoundException, InvalidEntityException{
+        Validator validator = validators.get(entityInput.getEntityCode());
         for(Entity entity : entities){
             if(entityInput.id == entity.id){
                 entities.remove(entity);
+                validator.validate(entityInput);
                 entities.add(entityInput.copy());
                 System.out.println("Entity updated successfully.");
                 return;
@@ -48,6 +55,15 @@ public class Database {
         }
         throw new EntityNotFoundException("Entity with same ID not found.");
     }
+
+    public static void registerValidator(int entityCode, Validator validator) {
+        if(validators.containsKey(entityCode)){
+            throw new IllegalArgumentException("Validator already exists.");
+        }else{
+            validators.put(entityCode, validator);
+        }
+    }
+
 
 
 
