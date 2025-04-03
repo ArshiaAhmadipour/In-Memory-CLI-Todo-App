7e38433519913faa2493db0d19ffd3480de475fc
ArshiaAHP <diniodduck@gmail.com>
2025-04-03 03:59:43 +0330

    fixed bug in main.

--- src/Main.java
+++ src/Main.java
@@ -26,170 +26,175 @@ public class Main {
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
-        while(true){
-            System.out.print("What do you want to do? ");
-            String command = inp.nextLine();
-            command = command.trim().toLowerCase();
-            switch (command){
-                case "add task": {
-                    System.out.print("Title: ");
-                    String title = inp.nextLine();
-                    System.out.print("Description: ");
-                    String description = inp.nextLine();
-                    System.out.print("Date(yyyy-mm-dd format): ");
-                    String dateStr = inp.nextLine();
-                    TaskService.addTask(title, description, dateStr);
-                    System.out.println("=====");
-                    break;
-                }
-                case "add step": {
-                    System.out.print("TaskID: ");
-                    UUID taskRef = UUID.fromString(inp.nextLine());
-                    System.out.print("Title: ");
-                    String title = inp.nextLine();
-                    StepService.addStep(taskRef, title);
-                    System.out.println("=====");
-                    break;
-                }
-                case "delete task": {
-                    System.out.print("ID: ");
-                    UUID id = UUID.fromString(inp.nextLine());
-                    TaskService.delete(id);
-                    System.out.println("=====");
-                    break;
-                }
-                case "update task": {
-                    System.out.print("Task ID: ");
-                    UUID id = UUID.fromString(inp.nextLine());
-                    System.out.print("Field (if date, YYYY-MM-DD format): ");
-                    String field = inp.nextLine();
-                    switch (field.toLowerCase()){
-                        case "title": {
-                            System.out.print("New Value: ");
-                            String newValue = inp.nextLine();
-                            TaskService.update(id, field, newValue, true, false);
-                            System.out.println("=====");
-                            break;
-                        }
-                        case "content":
-                        case "description": {
-                            System.out.print("New Value: ");
-                            String newValue = inp.nextLine();
-                            TaskService.update(id, field, newValue, false, false);
-                            System.out.println("=====");
-                            break;
-                        }
-                        case "date":
-                        case "due date": {
-                            System.out.print("New Value: ");
-                            String newValue = inp.nextLine();
-                            TaskService.update(id, field, newValue, false, true);
-                            System.out.println("=====");
-                            break;
-                        }
-                        case "status": {
-                            System.out.print("\nWhat is the status?\n\n1.Completed\n2.In progress\n3.Not started\nyour option: ");
-                            int option = inp.nextInt();
-                            inp.nextLine();
-                            TaskService.updateStatus(id, option);
-                            System.out.println("=====");
-                            break;
-                        }
-                        default:{
-                            System.out.println("invalid field.");
-                            break;
-                        }
+        while(true) {
+            try{
+                System.out.print("What do you want to do? ");
+                String command = inp.nextLine();
+                command = command.trim().toLowerCase();
+                switch (command){
+                    case "add task": {
+                        System.out.print("Title: ");
+                        String title = inp.nextLine();
+                        System.out.print("Description: ");
+                        String description = inp.nextLine();
+                        System.out.print("Date(yyyy-mm-dd format): ");
+                        String dateStr = inp.nextLine();
+                        TaskService.addTask(title, description, dateStr);
+                        System.out.println("=====");
+                        break;
                     }
-                    break;
-                }
-                case "update step": {
-                    System.out.print("Step ID: ");
-                    UUID id = UUID.fromString(inp.nextLine());
-                    System.out.print("Field: ");
-                    String field = inp.nextLine();
-                    switch (field.toLowerCase()){
-                        case "title": {
-                            System.out.print("New Value: ");
-                            String newValue = inp.nextLine();
-                            StepService.update(id, field, newValue, false);
-                            System.out.println("=====");
-                            break;
-                        }
-                        case "task ref":
-                        case "taskref": {
-                            System.out.println("New TaskID: ");
-                            String newValue = inp.nextLine();
-                            StepService.update(id, field, newValue, true);
-                            System.out.println("=====");
-                            break;
-                        }
-                        case "status": {
-                            System.out.print("\nWhat is the status?\n1.Completed\n2.Not started\nyour option: ");
-                            int option = inp.nextInt();
-                            inp.nextLine();
-                            StepService.updateStatus(id, option);
-                            System.out.println("=====");
-                            break;
-                        }
-                        default:{
-                            System.out.println("invalid field.");
-                            break;
-                        }
+                    case "add step": {
+                        System.out.print("TaskID: ");
+                        UUID taskRef = UUID.fromString(inp.nextLine());
+                        System.out.print("Title: ");
+                        String title = inp.nextLine();
+                        StepService.addStep(taskRef, title);
+                        System.out.println("=====");
+                        break;
                     }
-                    break;
-                }
-                case "delete step": {
-                    System.out.print("Step ID: ");
-                    UUID id = UUID.fromString(inp.nextLine());
-                    StepService.delete(id);
-                    System.out.println("=====");
-                    break;
-                }
-                case "get task by id":
-                case "get task": {
-                    System.out.print("Task ID: ");
-                    UUID id = UUID.fromString(inp.nextLine());
-                    TaskService.getTaskByID(id);
-                    System.out.println("=====");
-                    break;
-                }
-                case "get incomplete tasks":
-                case "get incomplete":
-                case "get itasks":
-                case "get tasks incomplete":{
-                    TaskService.getTaskIncomplete();
-                    System.out.println("=====");
-                    break;
-                }
-                case "get tasks":
-                case "get all":
-                case "get all tasks":
-                case "get tasks all":{
-                    TaskService.getTaskAll();
-                    System.out.println("=====");
-                    break;
-                }
+                    case "delete task": {
+                        System.out.print("ID: ");
+                        UUID id = UUID.fromString(inp.nextLine());
+                        TaskService.delete(id);
+                        System.out.println("=====");
+                        break;
+                    }
+                    case "update task": {
+                        System.out.print("Task ID: ");
+                        UUID id = UUID.fromString(inp.nextLine());
+                        System.out.print("Field (if date, YYYY-MM-DD format): ");
+                        String field = inp.nextLine();
+                        switch (field.toLowerCase()){
+                            case "title": {
+                                System.out.print("New Value: ");
+                                String newValue = inp.nextLine();
+                                TaskService.update(id, field, newValue, true, false);
+                                System.out.println("=====");
+                                break;
+                            }
+                            case "content":
+                            case "description": {
+                                System.out.print("New Value: ");
+                                String newValue = inp.nextLine();
+                                TaskService.update(id, field, newValue, false, false);
+                                System.out.println("=====");
+                                break;
+                            }
+                            case "date":
+                            case "due date": {
+                                System.out.print("New Value: ");
+                                String newValue = inp.nextLine();
+                                TaskService.update(id, field, newValue, false, true);
+                                System.out.println("=====");
+                                break;
+                            }
+                            case "status": {
+                                System.out.print("\nWhat is the status?\n\n1.Completed\n2.In progress\n3.Not started\nyour option: ");
+                                int option = inp.nextInt();
+                                inp.nextLine();
+                                TaskService.updateStatus(id, option);
+                                System.out.println("=====");
+                                break;
+                            }
+                            default:{
+                                System.out.println("invalid field.");
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                    case "update step": {
+                        System.out.print("Step ID: ");
+                        UUID id = UUID.fromString(inp.nextLine());
+                        System.out.print("Field: ");
+                        String field = inp.nextLine();
+                        switch (field.toLowerCase()){
+                            case "title": {
+                                System.out.print("New Value: ");
+                                String newValue = inp.nextLine();
+                                StepService.update(id, field, newValue, false);
+                                System.out.println("=====");
+                                break;
+                            }
+                            case "task ref":
+                            case "taskref": {
+                                System.out.println("New TaskID: ");
+                                String newValue = inp.nextLine();
+                                StepService.update(id, field, newValue, true);
+                                System.out.println("=====");
+                                break;
+                            }
+                            case "status": {
+                                System.out.print("\nWhat is the status?\n1.Completed\n2.Not started\nyour option: ");
+                                int option = inp.nextInt();
+                                inp.nextLine();
+                                StepService.updateStatus(id, option);
+                                System.out.println("=====");
+                                break;
+                            }
+                            default:{
+                                System.out.println("invalid field.");
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                    case "delete step": {
+                        System.out.print("Step ID: ");
+                        UUID id = UUID.fromString(inp.nextLine());
+                        StepService.delete(id);
+                        System.out.println("=====");
+                        break;
+                    }
+                    case "get task by id":
+                    case "get task": {
+                        System.out.print("Task ID: ");
+                        UUID id = UUID.fromString(inp.nextLine());
+                        TaskService.getTaskByID(id);
+                        System.out.println("=====");
+                        break;
+                    }
+                    case "get incomplete tasks":
+                    case "get incomplete":
+                    case "get itasks":
+                    case "get tasks incomplete":{
+                        TaskService.getTaskIncomplete();
+                        System.out.println("=====");
+                        break;
+                    }
+                    case "get tasks":
+                    case "get all":
+                    case "get all tasks":
+                    case "get tasks all":{
+                        TaskService.getTaskAll();
+                        System.out.println("=====");
+                        break;
+                    }
 
-                case "save":
-                case "save all":{
-                    try {
-                        Database.save();
-                        System.out.println("saved successfully.");
-                    } catch (IOException e) {
-                        System.out.println("cannot save.");
-                        e.printStackTrace();
+                    case "save":
+                    case "save all":{
+                        try {
+                            Database.save();
+                            System.out.println("saved successfully.");
+                        } catch (IOException e) {
+                            System.out.println("cannot save.");
+                            e.printStackTrace();
+                        }
+                        break;
                     }
-                    break;
+                    case "exit":{
+                        System.out.println("exiting program...");
+                        System.exit(0);
+                    }
+                    default:{
+                        System.out.println("invalid command, try again.");
+                        System.out.println("=====");
+                        break;
+                    }
                 }
-                case "exit":{
-                    System.out.println("exiting program...");
-                    System.exit(0);
-                }
-                default:{
-                    System.out.println("invalid command, try again.");
-                    System.out.println("=====");
-                    break;
-                }
+            } catch (RuntimeException e) {
+                System.out.println("Invalid ID.");
+                System.out.println(e.getMessage());
             }
         }
     }
